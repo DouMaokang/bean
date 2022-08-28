6350d2f7cb69760f6b5230289e95a5065a9aa49a
Maokang Dou <alecdou0001@gmail.com>
2022-08-28 15:40:37 +0800

    antlr cpp runtime setup

---
+++ antlr4_cpp/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "antlr4-runtime.h"
+#include "HelloLexer.h"
+#include "HelloParser.h"
+
+
+int main() {
+	std::ifstream stream;
+	stream.open("/Users/maokang/CLionProjects/bean/antlr4_cpp/input.txt");
+
+	antlr4::ANTLRInputStream input(stream);
+	antlrcpptest::HelloLexer lexer(&input);
+	antlr4::CommonTokenStream tokens(&lexer);
+	antlrcpptest::HelloParser parser(&tokens);
+	auto tree = parser.r();
+	std::cout << tree->toStringTree() << std::endl;
+	std::cout << tree->toString() << std::endl;
+	return 0;
+}
